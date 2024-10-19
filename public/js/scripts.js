29a7a8e6bf159a3fad800be4d49e92f1848351a9
Jens Christensen - Glodexa <jens@glodexa.com>
2024-10-20 00:14:39 +0200

    Översättning svensk profil

---
+++ public/js/scripts.js
@@ -0,0 +1,11 @@
+// Adding the Google Tag Manager Script to the head-tag and after the opening body tag
+
+// Add script for inserting page id
+
+// Add script for inserting global navigation
+
+// Assign classes for navigation to mark which (sub) page the user is on using the class w3-teal?
+
+// Adding aria classes for the current page
+
+// Chek out window loacation function......
